3ef5f1db3995e49716e2c871e6a4f5bab409eed6
m-evtimov96 <m.evtimov196@gmail.com>
2022-03-18 11:29:49 +0200

    Initial commit Add ticket model

---
+++ tickets/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+
+class Ticket(models.Model):
+
+    SOFTWARE_PROBLEM = 'software_problem'
+    HARDWARE_PROBLEM = 'hardware_problem'
+    NETWORK_ISSUE = 'network_issue'
+    INFOSEC_ISSUE = 'infosec_issue'
+
+    CATEGORY_CHOICES = {
+        (SOFTWARE_PROBLEM, 'Software problem'),
+        (HARDWARE_PROBLEM, 'Hardware problem'),
+        (NETWORK_ISSUE, 'Networking issue'),
+        (INFOSEC_ISSUE, 'Information Security issue'),
+    }
+
+    OPEN = 'open'
+    IN_PROGRESS = 'in_progress'
+    CLOSED = 'closed'
+
+    STATUS_CHOICES = {
+        (OPEN, 'Open'),
+        (IN_PROGRESS, 'In progress'),
+        (CLOSED, 'Closed'),
+    }
+
+    title = models.CharField(max_length=70)
+    content = models.TextField(max_length=3000)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    creator = models.ForeignKey(UserModel, related_name='tickets', on_delete=models.CASCADE)
+
+    worker = models.ForeignKey(UserModel, related_name='work_tickets', null=True, on_delete=models.SET_NULL)
